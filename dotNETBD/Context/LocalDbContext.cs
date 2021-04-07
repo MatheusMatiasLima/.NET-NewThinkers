4c3de13ff20f281c5d50575134c61fed7032b15c
Matheus Matias <matheusmatiaslima@hotmail.com>
2021-04-07 19:42:44 -0300

    :card_file_box: Criando conexão com o banco de dados

---
+++ dotNETBD/Context/LocalDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace dotNETBD.Context
+{
+    public class LocalDbContext : DbContext
+    {
+        public LocalDbContext (DbContextOptions<LocalDbContext> opt) : base (opt)
+        {
+
+        }
+
+        protected override void OnModelCreating (ModelBuilder modelBuilder)
+        {
+            //para relacoes 
+        }
+
+    }
+}
---
+++ dotNETBD/Entities/Pessoa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotNETBD.Entities
+{
+    public class Pessoa
+    {
+        [Key]
+        public int id { get; set; }
+
+        public string nome { get; set; }
+        public int cpf { get; set; }
+    }
+}
